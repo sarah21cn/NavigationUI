2e23b2063d746021035a816a56a2fdd8729d7737
Yin Shan <ys_sarah@163.com>
2020-06-15 15:02:17 +0800

    add BottomNavBarActivity
    Signed-off-by: Yin Shan <ys_sarah@163.com>

--- app/src/main/java/com/ys/navigationui/bottomnavbar/BottomNavBarActivity.java
+++ app/src/main/java/com/ys/navigationui/bottomnavbar/BottomNavBarActivity.java
@@ -3,6 +3,12 @@ package com.ys.navigationui.bottomnavbar;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ys.navigationui.R;
 
 /**
  * Created by yinshan on 2020/6/15.
@@ -12,5 +18,10 @@ public class BottomNavBarActivity extends AppCompatActivity {
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_bottom_navbar);
+
+    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+    BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+    NavigationUI.setupWithNavController(bottomNavigationView, navController);
   }
 }
