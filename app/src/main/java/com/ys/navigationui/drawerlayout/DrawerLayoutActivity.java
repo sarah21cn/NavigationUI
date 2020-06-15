2063d146f7c15c6fb36a5c0c0b3a51396aac4bbd
Yin Shan <ys_sarah@163.com>
2020-06-15 14:47:54 +0800

    add drawerLayoutActivity
    Signed-off-by: Yin Shan <ys_sarah@163.com>

--- app/src/main/java/com/ys/navigationui/drawerlayout/DrawerLayoutActivity.java
+++ app/src/main/java/com/ys/navigationui/drawerlayout/DrawerLayoutActivity.java
@@ -3,14 +3,40 @@ package com.ys.navigationui.drawerlayout;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.ys.navigationui.R;
 
 /**
  * Created by yinshan on 2020/6/15.
  */
 public class DrawerLayoutActivity extends AppCompatActivity {
 
+  private NavController navController;
+  private AppBarConfiguration appBarConfiguration;
+
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_drawer_layout);
+
+    DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+    navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+    appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).setDrawerLayout(drawerLayout).build();
+    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+    NavigationView navigationView = findViewById(R.id.navigation_view);
+    NavigationUI.setupWithNavController(navigationView, navController);
+  }
+
+  @Override
+  public boolean onSupportNavigateUp() {
+    return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
   }
 }
---
+++ app/src/main/java/com/ys/navigationui/drawerlayout/DrawerLayoutMainFragment.java
@@ -0,0 +1,25 @@
+package com.ys.navigationui.drawerlayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.ys.navigationui.R;
+
+/**
+ * Created by yinshan on 2020/6/15.
+ */
+public class DrawerLayoutMainFragment extends Fragment {
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_second_main, container, false);
+    return view;
+  }
+}
